lazyoop/immwrt-mt798x-Stitch-Enhanced-luci

luci-app-dockerman: model/docker: socket path variable name
Signed-off-by: Florian Eckert <fe@dev.tdt.de>

applications/luci-app-dockerman/luasrc/model/docker.lua
@@ -272,7 +272,7 @@ end
 _docker.new = function()
 	local host = nil
 	local port = nil
-	local socket = nil
+	local socket_path = nil
 	local debug_path = nil
 
 	local remote = uci:get_bool("dockerd", "globals", "remote_endpoint")
@@ -280,7 +280,7 @@ _docker.new = function()
 		host = uci:get("dockerd", "globals", "remote_host") or nil
 		port = uci:get("dockerd", "globals", "remote_port") or nil
 	else
-		socket = uci:get("dockerd", "globals", "socket_path") or "/var/run/docker.sock"
+		socket_path = uci:get("dockerd", "globals", "socket_path") or "/var/run/docker.sock"
 	end
 
 	local debug = uci:get_bool("dockerd", "globals", "debug")
@@ -291,7 +291,7 @@ _docker.new = function()
 	_docker.options = {
 		host = host,
 		port = port,
-		socket_path = socket,
+		socket_path = socket_path,
 		debug = debug,
 		debug_path = debug_path,
 		status_path = uci:get("dockerd", "globals", "status_path") or "/tmp/.docker_status"

hue715/luci-app-dockerman

(cherry picked from commit https://github.com/openwrt/luci/commit/3794693675fc993af3112cfe7cdf990e7cc7e891 )

applications/luci-app-dockerman/luasrc/model/docker.lua
@@ -272,7 +272,7 @@ end
 _docker.new = function()
 	local host = nil
 	local port = nil
-	local socket = nil
+	local socket_path = nil
 	local debug_path = nil
 
 	local remote = uci:get_bool("dockerd", "dockerman", "remote_endpoint")
@@ -280,8 +280,7 @@ _docker.new = function()
 		host = uci:get("dockerd", "dockerman", "remote_host") or nil
 		port = uci:get("dockerd", "dockerman", "remote_port") or nil
 	else
-		socket = uci:get("dockerd", "dockerman", "socket_path") or "/var/run/docker.sock"
-	end
+		socket_path = uci:get("dockerd", "dockerman", "socket_path") or "/var/run/docker.sock"
 
 	local debug = uci:get_bool("dockerd", "dockerman", "debug")
 	if debug then
@@ -291,7 +290,7 @@ _docker.new = function()
 	_docker.options = {
 		host = host,
 		port = port,
-		socket_path = socket,
+		socket_path = socket_path,
 		debug = debug,
 		debug_path = debug_path,
 		status_path = uci:get("dockerd", "dockerman", "status_path") or "/tmp/.docker_status"